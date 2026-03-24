914fca7ffdba4d448a711f78625126a27061d586
Orçun Türkol <orcun.turkol@hangikredi.com>
2026-03-24 18:12:01 +0300

    git pull

--- generate-migration.sh
+++ generate-migration.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 dotnet tool install --global dotnet-ef
 export PATH="$PATH:/root/.dotnet/tools"
+dotnet restore src/Cato.API/Cato.API.csproj
 dotnet-ef migrations add AddPicsChangeHistory --project src/Cato.Infrastructure --startup-project src/Cato.API
